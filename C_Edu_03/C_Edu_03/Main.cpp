452eb13a5ec4f3478118cd048a403743804276f4
grey <grey.hande@gmail.com>
2022-08-03 09:55:44 +0900

    Chapter 03

--- C_Edu_03/C_Edu_03/Main.cpp
+++ C_Edu_03/C_Edu_03/Main.cpp
@@ -124,7 +124,7 @@ int main(void)
 	int age;// 나이 : 정수형
 	double height; // 키 : 실수형
 
-	printf("나이와 키를 입력하세요 : ") // 입력 안내 메시지 출력
+	printf("나이와 키를 입력하세요 : "); // 입력 안내 메시지 출력
 	scanf("%d%lf", &age, &height); // 나이와 키를 함께 입력
 	printf("나이는 %d살, 키는 %.1fcm입니다\n", age, height); // 입력값 출력
 
