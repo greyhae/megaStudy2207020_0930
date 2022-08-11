08b82d901d962e0c103178e151260515df981804
grey <grey.hande@gmail.com>
2022-08-11 10:54:00 +0900

    Chapter 09 : 포인터

--- C_Edu_09/C_Edu_09/Main.cpp
+++ C_Edu_09/C_Edu_09/Main.cpp
@@ -17,6 +17,7 @@ int main(void)
 
 		// ** 포인터 변수
 		int* pNumber;
+		// ** 포인터 변수는 [*]을 붙여준다.
 		// ** 포인터 변수는 일반 변수의 주소를 갖는다.
 	}
 
@@ -59,6 +60,84 @@ int main(void)
 		printf_s("Pointer 가 가르키는 값 : %p\n", pNumber);
 		printf_s("Pointer 의 주소 : %p\n", &pNumber);
 		printf_s("\n");
+
+	}
+
+	{
+		// ** 포인터 변수와 일반 변수는 데이터를 공유한다.
+		// ** 검증
+
+		int Number = 10;
+
+		// ** 포인터 변수는 일반 변수의 주소를 갖는다.
+		int* pNumber = &Number;
+
+		// ** 포인터 변수가 데이터를 출력할 때에는 항목 앞에 [*] 를 붙인다.
+		printf_s("Number 값 : %d\n", Number);
+		printf_s("Pointer 가 가르키는 값 : %d\n", *pNumber);
+		printf_s("\n");
+
+		// ** 일반 변수의 값만 변경
+		Number = 100;
+
+		printf_s("Number 값 : %d\n", Number);
+		printf_s("Pointer 가 가르키는 값 : %d\n", *pNumber);
+		printf_s("\n");
+
+		// ** 포인터 변수의 값만 변경
+		*pNumber = 1000;
+
+		printf_s("Number 값 : %d\n", Number);
+		printf_s("Pointer 가 가르키는 값 : %d\n", *pNumber);
+		printf_s("\n");
+	}
+
+	{
+		// ** swap
+		
+		int a = 10;
+		int b = 20;
+
+		printf("a : %d\n", a);
+		printf("b : %d\n", b);
+
+		if (true)
+		{
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		printf("a : %d\n", a);
+		printf("b : %d\n", b);
+	}
+
+	{
+		// ** swap
+
+		int a = 10;
+		int b = 20;
+		
+		a ^= b;
+		b ^= a;
+
+		printf("a : %d\n", a);
+		printf("b : %d\n", b);
+	}
+
+	{
+		// ** 포인터의 크기는 개발환경에 따라 그 크기가 달라진다.
+		int Number = 10;
+		long long lNum = 10;
+		long long* Pointer = &lNum;
+
+
+		// ** 포인터의 크기
+		// x86 = 4byte
+		// x64 = 8byte
+
+		printf("%d\n\n", sizeof(Number));
+		printf("%d\n\n", sizeof(lNum));
+		printf("%d\n\n", sizeof(Pointer));
 
 	}
 
