e21a5f3cf004cc4efcaccc723f32c96ca4126082
greyhae <109651403+greyhae@users.noreply.github.com>
2022-10-24 17:57:41 +0900

    Add files via upload

---
+++ C++_Edu_11/C++_Edu_11/Logo.cpp
@@ -0,0 +1,58 @@
+﻿#include "Logo.h"
+#include "SceneManager.h"
+#include "Child.h"
+#include "CursorManager.h"
+
+//http://patorjk.com/software/taag/
+Logo::Logo()
+{
+
+}
+
+Logo::~Logo()
+{
+
+}
+
+void Logo::Start(void)
+{
+	//cout << "Logo Start" << endl;
+	Info.Position = Vector3(0.0f, 0.0f, 0.0f);
+	Info.Rotation = Vector3(0.0f, 0.0f, 0.0f);
+	Info.Scale = Vector3(0.0f, 0.0f, 0.0f);
+	Color = 0;
+	SwitchColor = 0;
+	Time = GetTickCount64();
+	DelayTime = 50;
+
+
+}
+void Logo::Update(void)
+{
+	if (GetAsyncKeyState(VK_RETURN))
+		SceneManager::GetInstance()->SetScene(MENUID);
+
+
+	if (Time + DelayTime < GetTickCount64())
+	{
+		Time = GetTickCount64();
+
+		//++SwitchColor;
+	}
+}
+
+void Logo::Render(void)
+{
+	CursorManager::Renderer(Info.Position.x, Info.Position.y + 0, "	⋱ ⋮ ⋰", 5);
+	CursorManager::Renderer(Info.Position.x, Info.Position.y + 1, "⋯ ◯ ⋯ ︵ 　　　　　　^v^", 5);
+	CursorManager::Renderer(Info.Position.x, Info.Position.y + 2, "¸︵︵( ░░ )︵.︵.︵　　", 5);
+	CursorManager::Renderer(Info.Position.x, Info.Position.y + 3, "(''░░░░░░ ') ░░░' )", 5);
+	CursorManager::Renderer(Info.Position.x, Info.Position.y + 4, "'''︶'¯'︶''︶'︶'　^v^　　^v^", 5);
+
+}
+
+void Logo::Release(void)
+{
+
+
+}
